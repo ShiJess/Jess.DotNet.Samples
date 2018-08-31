71bd37285139d28b9e399910803bf4143e630232
Jess Smith <shisujieit@gmail.com>
2018-08-31 23:16:42 +0800

    值类型比较——等值，不同地址引用；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -3,24 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace 练习4_3
+namespace 练习4_4
 {
-    public class a
-    {
-        public a()
-        {
-        }
-    }
-    public class b : a
-    {
-        public b()
-        {
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
+            int i = 5;
+            int j = 5;
+            if (object.ReferenceEquals(i, j))
+                Console.WriteLine("Equal");
+            else
+                Console.WriteLine("Not Equal");
+            i = j;
+            Console.WriteLine(object.ReferenceEquals(i, j));
         }
     }
 }
