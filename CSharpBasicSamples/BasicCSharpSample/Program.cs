24c9531d6616180028012c1c94d8a812aa459ba3
Jess Smith <shisujieit@gmail.com>
2018-08-31 23:18:54 +0800

    类属性及枚举示例；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -3,23 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace 练习4_11
+namespace 示例3_3
 {
-    public interface MyInterface
+    //枚举定义性别
+    public enum Genders
     {
-        string Name { get; }
+        Female = 0,
+        Male = 1
     }
-    class MyClass : MyInterface
+    public class Person
     {
-        public string Name { get { return "only a test!"; } }
+        private String _Name;//姓名字段
+        public String Name//姓名属性
+        {
+            get
+            {
+                return this._Name;
+            }
+            set
+            {
+                this._Name = value.Trim();
+            }
+        }
+        private int _Age;//年龄字段
+        public int Age//年龄属性
+        {
+            get
+            {
+                return this._Age;
+            }
+            set
+            {
+                //如果年龄超出1-120，则用默认值20
+                if ((value > 120) || (value < 1))
+                    this._Age = 20;
+                else
+                    this._Age = value;
+            }
+        }
+        private Genders _Gender;
+        public Genders Gender
+        {
+            get
+            {
+                return this._Gender;
+            }
+            set
+            {
+                this._Gender = value;
+            }
+        }
     }
     class Program
     {
-
         static void Main(string[] args)
         {
-            MyInterface x = new MyClass();
-            Console.WriteLine(x.Name);
+            Person aPerson = new Person();
+            aPerson.Name = "zhangsan";
+            aPerson.Gender = Genders.Male;
+            aPerson.Age = 30;
+            PrintPerson(aPerson);
+        }
+        static void PrintPerson(Person aPerson)
+        {
+            System.Console.WriteLine("姓名：{0}性别：{1}年龄：{2}", aPerson.Name, aPerson.Gender, aPerson.Age);
         }
     }
 }
