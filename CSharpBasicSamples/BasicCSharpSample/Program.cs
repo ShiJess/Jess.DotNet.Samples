f90a7863e73bd6d6cf4859365f219bf08e980bc2
Jess Smith <shisujieit@gmail.com>
2018-08-31 23:08:40 +0800

    数字格式化输出——金额格式化；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-public class Demo
+public class format
 {
     public static void Main()
     {
-        short x = 1;
-        decimal y = 2.2m;
-        double z = 3.3;
-        Console.WriteLine("结果是：");
-        //Console.WriteLine(x + y + z);   //错误，无法进行隐式类型转换
-        Console.WriteLine(x + y);
-        Console.WriteLine(x + z);
-        //Console.WriteLine(y+z);   //错误，decimal类型无法隐式转换
+        decimal x = 11111111.976M;
+        decimal y = 123456789123456789m;
+        Console.Write("结果是：");
+        Console.WriteLine("{0:C}", x);
+        Console.Write("结果是：");
+        Console.WriteLine("{0:C}", y);
+
     }
 }
