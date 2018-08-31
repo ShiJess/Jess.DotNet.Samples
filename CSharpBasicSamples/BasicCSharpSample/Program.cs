0e4c3bc7887024f5bb2832722b2c8b9ba1613a00
Jess Smith <shisujieit@gmail.com>
2018-08-31 23:14:57 +0800

    类继承；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -3,34 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace 练习4_1
+namespace 练习4_3
 {
-    class A
+    public class a
     {
-        public A()
+        public a()
         {
-            PrintFields();
         }
-        public virtual void PrintFields() { }
     }
-    class B : A
+    public class b : a
     {
-        int x = 1;
-        int y;
-        public B()
+        public b()
         {
-            y = -1;
-        }
-        public override void PrintFields()
-        {
-            Console.WriteLine("x={0},y={1}", x, y);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            B b = new B();
         }
     }
 }
