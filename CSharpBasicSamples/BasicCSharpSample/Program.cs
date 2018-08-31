e7f954a7e24f94f6d67aa4a27eb7ead54dcdf5f5
Jess Smith <shisujieit@gmail.com>
2018-08-31 23:05:18 +0800

    不同精度数字运算2；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -3,9 +3,10 @@ class Demo
 {
     public static void Main()
     {
-        int val1 = 8;
-        float val2 = 17.6f;
-        short val3 = 12;
-        Console.WriteLine("结果是:{0}", val1 * val2 / val3);
+        int x = 2;
+        float y = 2.2f;
+        short z = 12;
+        double k = 1.78E+10;
+        Console.WriteLine("结果是：{0}", x + y + z + k);
     }
 }
