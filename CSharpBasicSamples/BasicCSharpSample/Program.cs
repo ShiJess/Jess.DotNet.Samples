c7c03ccff7edf4d8e8459b346d6955578161095a
Jess Smith <shisujieit@gmail.com>
2018-08-31 23:17:25 +0800

    接口定义实现使用；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace 练习4_4
+namespace 练习4_11
 {
+    public interface MyInterface
+    {
+        string Name { get; }
+    }
+    class MyClass : MyInterface
+    {
+        public string Name { get { return "only a test!"; } }
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
-            int i = 5;
-            int j = 5;
-            if (object.ReferenceEquals(i, j))
-                Console.WriteLine("Equal");
-            else
-                Console.WriteLine("Not Equal");
-            i = j;
-            Console.WriteLine(object.ReferenceEquals(i, j));
+            MyInterface x = new MyClass();
+            Console.WriteLine(x.Name);
         }
     }
 }
