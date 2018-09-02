1c3bb58a926e76b2a8b21e3308d17c8ecad4f344
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:41:12 +0800

    二维数组-冒泡排序；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -5,17 +5,46 @@ using System.Text;
 namespace HelloWangyexx
 {
     /// <summary>
-    /// 此示例演示使用 C# 中的一维数组
+    /// 本程序演示使用二重循环实现数组的冒泡排序算法
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            int[] array = new int[5] { 0, 1, 2, 3, 4 };// 声明并初始化一维数组
-            for (int i = 0; i < array.Length; i++)    // 输出数组中的所有元素
+            int[] scores = new int[5];
+            int i, j;  // 循环变量
+            int temp;  // 临时变量
+
+            // 读入成绩
+            Console.WriteLine("请输入5个学员的成绩：");
+            for (i = 0; i < 5; i++)
             {
-                Console.WriteLine(array[i]);
+                Console.WriteLine("请输入第{0}个学员的成绩：", i + 1);
+                scores[i] = int.Parse(Console.ReadLine());//类型转换  
             }
+
+            // 开始排序
+            for (i = 0; i < scores.Length - 1; i++)
+            {
+                for (j = 0; j < scores.Length - 1 - i; j++)
+                {
+                    if (scores[j] > scores[j + 1])
+                    {
+                        // 交换元素
+                        temp = scores[j];
+                        scores[j] = scores[j + 1];
+                        scores[j + 1] = temp;
+                    }
+                }
+            }
+
+            // 排序后输出
+            Console.WriteLine("排序后的成绩为：");
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("{0}\t", scores[i]);
+            }
+
             Console.ReadLine();
         }
     }
