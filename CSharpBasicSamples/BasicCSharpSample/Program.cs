7c88154e000bbe31167b3263df4d874027516431
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:44:04 +0800

    字符串 处理；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -5,32 +5,44 @@ using System.Text;
 namespace HelloWangyexx
 {
     /// <summary>
-    /// 此示例演示使用方法交换两个数
+    /// 此程序演示字符串的常用处理方法，从邮箱地址中提取用户名
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            int num1, num2;
-            Console.WriteLine("请输入两个数字：");
-            num1 = int.Parse(Console.ReadLine());
-            num2 = int.Parse(Console.ReadLine());
+            string email;  // 邮箱地址
+            string choice; // 用户的选择
+            string name;   // 邮箱用户名
 
-            Console.WriteLine("交换前两个数的分别为：{0}和{1}", num1, num2);
+            do
+            {
+                Console.WriteLine("请输入你的邮箱地址：");
+                email = Console.ReadLine();
 
-            // 交换两个数的值
-            Swap(ref num1, ref num2);
+                Console.WriteLine("你的邮箱地址是:{0}", email); //前两步中使用                
 
-            Console.WriteLine("交换后两个数的分别为：{0}和{1}", num1, num2);
+                // 抽取邮箱用户名
+                int position = email.IndexOf("@");    // 完善的第三步：使用IndexOf
+                if (position > 0)
+                {
+                    name = email.Substring(0, position);  // 完善的第三步：使用Substring
+
+                    // 输出邮箱用户名
+                    Console.WriteLine("你的邮箱用户名是：{0}", name);
+                }
+                else
+                {
+                    Console.WriteLine("你的邮箱格式错误！");
+                }
+
+                Console.WriteLine("是否要继续？yes/no");           // 第一步
+                choice = Console.ReadLine();
+
+                choice = choice.Trim().ToLower(); //完善的第二步：使用Trim 、ToLower方法
+            } while (choice.Equals("yes"));  // 完善的第一步：使用Equals判断
+
             Console.ReadLine();
-        }
-
-        private static void Swap(ref int num1, ref int num2)
-        {
-            int temp;      // 中间变量
-            temp = num1;
-            num1 = num2;
-            num2 = temp;
         }
     }
 }
