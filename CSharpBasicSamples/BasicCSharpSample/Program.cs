156b9f396b26e34313c2ae273c40d42542f0445e
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:33:12 +0800

    控制台程序输入输出 ；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -5,13 +5,26 @@ using System.Text;
 namespace HelloWangyexx
 {
     /// <summary>
-    /// 此示例演示第一个 C# 程序
+    /// 此代码演示向控制台输出
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            string course = "C#";  //课程名称
+
+            // 向控制台输出信息
             Console.WriteLine("Hello Wangyexx .NET");
+
+            // 向控制台输出单个变量
+            Console.WriteLine(course);
+
+            // + 连接输出字符串
+            Console.WriteLine("我的课程名称是：" + course);
+
+            // 使用格式字符串
+            Console.WriteLine("我的课程名称是：{0}", course);
+
             Console.ReadLine();
         }
     }
