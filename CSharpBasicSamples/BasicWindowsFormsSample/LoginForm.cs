95a40d31351a40593a9d6e7ee8393081dcd84273
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:57:46 +0800

    登录窗体；

---
+++ CSharpBasicSamples/BasicWindowsFormsSample/LoginForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MySchool
+{
+    /// <summary>
+    /// 登录窗体
+    /// </summary>
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        // 点击取消按钮，关闭应用程序
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // 点击登录按钮时，设置用户名和登录类型
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtLoginId.Text == "")  // 判断“用户名”文本框中的字符串是否为空
+            {
+                MessageBox.Show("请输入用户姓名");
+                MessageBox.Show("请输入用户姓名", "输入提示");
+                MessageBox.Show("请输入用户姓名", "输入提示", MessageBoxButtons.OKCancel);
+                MessageBox.Show("请输入用户姓名", "输入提示",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+        }       
+    }
+}
---
+++ CSharpBasicSamples/BasicWindowsFormsSample/LoginForm.designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MySchool
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.txtLoginId = new System.Windows.Forms.TextBox();
+            this.txtLoginPwd = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.cboLoginType = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtLoginId
+            // 
+            this.txtLoginId.Location = new System.Drawing.Point(214, 148);
+            this.txtLoginId.Name = "txtLoginId";
+            this.txtLoginId.Size = new System.Drawing.Size(122, 21);
+            this.txtLoginId.TabIndex = 0;
+            // 
+            // txtLoginPwd
+            // 
+            this.txtLoginPwd.Location = new System.Drawing.Point(214, 182);
+            this.txtLoginPwd.Name = "txtLoginPwd";
+            this.txtLoginPwd.PasswordChar = '*';
+            this.txtLoginPwd.Size = new System.Drawing.Size(122, 21);
+            this.txtLoginPwd.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnLogin.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLogin.Location = new System.Drawing.Point(142, 262);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(86, 30);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "登　录";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCancel.Location = new System.Drawing.Point(244, 262);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(89, 30);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "取　消";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // cboLoginType
+            // 
+            this.cboLoginType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboLoginType.FormattingEnabled = true;
+            this.cboLoginType.Items.AddRange(new object[] {
+            "管理员",
+            "学员",
+            "教员"});
+            this.cboLoginType.Location = new System.Drawing.Point(214, 217);
+            this.cboLoginType.Name = "cboLoginType";
+            this.cboLoginType.Size = new System.Drawing.Size(122, 20);
+            this.cboLoginType.TabIndex = 2;
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(496, 300);
+            this.Controls.Add(this.cboLoginType);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtLoginPwd);
+            this.Controls.Add(this.txtLoginId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "登录";
+            this.TransparencyKey = System.Drawing.Color.DarkRed;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLoginId;
+        private System.Windows.Forms.TextBox txtLoginPwd;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ComboBox cboLoginType;
+    }
+}
