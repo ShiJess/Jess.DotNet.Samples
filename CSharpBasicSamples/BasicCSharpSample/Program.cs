3a18417aabb7178b2c1ee8b9e94c199aa09c1340
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:35:21 +0800

    switch结构；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -5,42 +5,37 @@ using System.Text;
 namespace HelloWangyexx
 {
     /// <summary>
-    /// 此程序演示如何使用嵌套 if 结构
+    /// 此代码演示如何使用switch语句
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            int price = 4000;  // 机票的原价
-            int month;         // 出行的月份
-            int type;          // 头等舱为1,经济舱为2
+            string name1 = "张三";    // 第一个学员姓名
+            string name2 = "李四";    // 第二个学员姓名
+            string subject1 = "C#";   // 第一个学员考试科目
+            string subject2 = "Java"; // 第二个学员考试科目
+            int score1 = 91;          // 第一个学员考试分数
+            int score2 = 89;          // 第二个学员考试分数
 
-            Console.WriteLine("请输入您出行的月份：1-12");
-            month = int.Parse(Console.ReadLine());
-            Console.WriteLine("请问您选择头等舱还是经济舱？头等舱输入1，经济舱输入2");
-            type = int.Parse(Console.ReadLine());
+            // 输入提示
+            Console.WriteLine("请选择输出哪个学员的信息：张三 / 李四");
 
-            if (month >= 5 && month <= 10)  // 旺季
+            string choice = Console.ReadLine();  // 接收输入
+
+            // 判断输入，选择输出
+            Console.WriteLine("姓名\t科目\t成绩");
+            switch (choice)
             {
-                if (type == 1)        // 头等舱
-                {
-                    Console.WriteLine("您的机票价格为：{0}", price * 0.9);
-                }
-                else if (type == 2)  // 经济舱
-                {
-                    Console.WriteLine("您的机票价格为：{0}", price * 0.75);
-                }
-            }
-            else  // 淡季
-            {
-                if (type == 1)       // 头等舱
-                {
-                    Console.WriteLine("您的机票价格为：{0}", price * 0.6);
-                }
-                else if (type == 2)  // 经济舱
-                {
-                    Console.WriteLine("您的机票价格为：{0}", price * 0.3);
-                }
+                case "张三":
+                    Console.WriteLine("{0}\t{1}\t{2}", name1, subject1, score1);
+                    break;
+                case "李四":
+                    Console.WriteLine("{0}\t{1}\t{2}", name2, subject2, score2);
+                    break;
+                default:
+                    Console.WriteLine("抱歉！没有你要找的学员！");
+                    break;
             }
 
             Console.ReadLine();
