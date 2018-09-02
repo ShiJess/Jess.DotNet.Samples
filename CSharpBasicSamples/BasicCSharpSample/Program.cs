2a2966969e03e4e927df2b0264dd8a7f891024f3
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:36:57 +0800

    foreach结构；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -5,39 +5,22 @@ using System.Text;
 namespace HelloWangyexx
 {
     /// <summary>
-    /// 此示例演示贯穿 case 的break
+    /// 此程序演示如何使用foreach循环
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("请输入当前的时间：");
-            string time = Console.ReadLine(); // 接受输入
+            // 输入提示            
+            Console.WriteLine("请输入一个字符串：");
 
-            switch (time)　　// 根据时间输出
+            // 从控制台读入字符串
+            string line = Console.ReadLine();
+
+            // 循环输出字符串中的字符
+            foreach (char c in line)
             {
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "10":
-                    Console.WriteLine("上午好");
-                    break;
-                case "11":
-                case "12":
-                case "13":
-                    Console.WriteLine("中午好");
-                    break;
-                case "14":
-                case "15":
-                case "16":
-                case "17":
-                case "18":
-                    Console.WriteLine("下午好");
-                    break;
-                default:
-                    Console.WriteLine("休息时间");
-                    break;
+                Console.WriteLine(c);
             }
 
             Console.ReadLine();
