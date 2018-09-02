47e6ee17018173b280fed321b3936a75e2edba77
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:44:53 +0800

    字符串分割连接；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -5,42 +5,34 @@ using System.Text;
 namespace HelloWangyexx
 {
     /// <summary>
-    /// 此程序演示字符串的常用处理方法，从邮箱地址中提取用户名
+    /// 此示例演示使用Split()和Join()方法分割和连接字符串
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            string email;  // 邮箱地址
-            string choice; // 用户的选择
-            string name;   // 邮箱用户名
+            string inputString;  // 输入的字符串
+            string[] splitStrings; // 分割后的字符串数组
+            string joinString;  // 连接后的新字符串
 
-            do
+            // 输入提示
+            Console.WriteLine("请输入一串字符串，用空格分隔单词：");
+            inputString = Console.ReadLine();//  接收输入
+
+            splitStrings = inputString.Split(' ');  // 以空格作为分隔符分割字符串
+
+            // 将分割后的字符串输出
+            Console.WriteLine("\n分割后的字符串为：");
+            foreach (string s in splitStrings)
             {
-                Console.WriteLine("请输入你的邮箱地址：");
-                email = Console.ReadLine();
+                Console.WriteLine(s);
+            }
 
-                Console.WriteLine("你的邮箱地址是:{0}", email); //前两步中使用                
+            // 将分割后的字符串使用下划线连接在一起
+            joinString = string.Join("_", splitStrings);
 
-                // 抽取邮箱用户名
-                int position = email.IndexOf("@");    // 完善的第三步：使用IndexOf
-                if (position > 0)
-                {
-                    name = email.Substring(0, position);  // 完善的第三步：使用Substring
-
-                    // 输出邮箱用户名
-                    Console.WriteLine("你的邮箱用户名是：{0}", name);
-                }
-                else
-                {
-                    Console.WriteLine("你的邮箱格式错误！");
-                }
-
-                Console.WriteLine("是否要继续？yes/no");           // 第一步
-                choice = Console.ReadLine();
-
-                choice = choice.Trim().ToLower(); //完善的第二步：使用Trim 、ToLower方法
-            } while (choice.Equals("yes"));  // 完善的第一步：使用Equals判断
+            // 将连接后的字符串输出
+            Console.WriteLine("\n连接后的新字符串为：{0}", joinString);
 
             Console.ReadLine();
         }
