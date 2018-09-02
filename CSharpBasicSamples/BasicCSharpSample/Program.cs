e1487bb791ad8845b15cc54cc4efaebecf8365ec
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:34:01 +0800

    控制台程序输入输出；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -5,25 +5,32 @@ using System.Text;
 namespace HelloWangyexx
 {
     /// <summary>
-    /// 此代码演示向控制台输出
+    /// 此示例演示向控制台输出学员信息，在一行当中使用多个{0}、{1}...
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            string course = "C#";  //课程名称
+            string name1;  // 第一个学员姓名
+            string name2;  // 第二个学员姓名
+            int age1;      // 第一个学员年龄
+            int age2;      // 第二个学员年龄            
 
-            // 向控制台输出信息
-            Console.WriteLine("Hello Wangyexx .NET");
+            // 输入第一个学员提示
+            Console.WriteLine("请输入第一个学员的姓名：");
+            name1 = Console.ReadLine();
+            Console.WriteLine("请输入第一个学员的年龄：");
+            age1 = int.Parse(Console.ReadLine());
 
-            // 向控制台输出单个变量
-            Console.WriteLine(course);
+            // 输入第二个学员提示
+            Console.WriteLine("请输入第二个学员的姓名：");
+            name2 = Console.ReadLine();
+            Console.WriteLine("请输入第二个学员的年龄：");
+            age2 = int.Parse(Console.ReadLine());
 
-            // + 连接输出字符串
-            Console.WriteLine("我的课程名称是：" + course);
-
-            // 使用格式字符串
-            Console.WriteLine("我的课程名称是：{0}", course);
+            // 输出学员信息
+            Console.WriteLine("第一个学员的姓名" + name1 + ",年龄" + age1 + "岁");
+            Console.WriteLine("第二个学员的姓名{0},年龄{1}岁", name2, age2);
 
             Console.ReadLine();
         }
