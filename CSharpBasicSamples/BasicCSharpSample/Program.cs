49e0e0bfb660ee8be9bc1905b63876796b34aeb9
Jess Smith <shisujieit@gmail.com>
2018-09-02 09:37:38 +0800

    for结构；

--- CSharpBasicSamples/BasicCSharpSample/Program.cs
+++ CSharpBasicSamples/BasicCSharpSample/Program.cs
@@ -5,22 +5,31 @@ using System.Text;
 namespace HelloWangyexx
 {
     /// <summary>
-    /// 此程序演示如何使用foreach循环
+    /// 此程序演示二重循环应用
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            // 输入提示            
-            Console.WriteLine("请输入一个字符串：");
+            int i, j;      // 循环变量
+            int sum = 0;   // 总分
+            int average;   // 平均分
+            int score;     // 输入的分数
 
-            // 从控制台读入字符串
-            string line = Console.ReadLine();
-
-            // 循环输出字符串中的字符
-            foreach (char c in line)
+            // 外层循环控制逐个计算每个班级
+            for (i = 0; i < 3; i++)
             {
-                Console.WriteLine(c);
+                sum = 0;  // 总分计数清零
+                Console.WriteLine("\n请输入第{0}个班的成绩", i + 1);
+                // 内层循环计算每个班级的总分
+                for (j = 0; j < 4; j++)
+                {
+                    Console.Write("第{0}个学员的成绩：", j + 1);
+                    score = int.Parse(Console.ReadLine());
+                    sum = sum + score;
+                }
+                average = sum / 4;
+                Console.WriteLine("第{0}个班的平均分为：{1}分", i + 1, average);
             }
 
             Console.ReadLine();
